83089354fe59fd933381072d02fa34dfea8e390f
rtof <zuichuan@msn.com>
2022-09-22 10:27:45 -0300

    fixing objectId error

--- api/routes/University/getUniversityById.js
+++ api/routes/University/getUniversityById.js
@@ -1,18 +1,21 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const University = require('../../models/University.js');
 
 const getUniversityById = express.Router();
 
 getUniversityById.get('/:id', async (req, res) => {
   try {
-    const university = await University.findOne({ _id: req.params.id });
+    if (mongoose.isValidObjectId(req.params.id)) {
+      const university = await University.findOne({ _id: req.params.id });
 
-    if (!university) {
-      res.status(422).json({ message: 'Record not found!' });
-      return
+      if (!university)
+        res.status(422).json({ message: 'Record not found!' });
+
+      res.status(200).json(university);
+    } else {
+      res.status(400).json({ message: 'invalid id' })
     }
-
-    res.status(200).json(university);
   } catch (error) {
     res.status(500).json({ erro: error });
   }
