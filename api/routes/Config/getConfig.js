b7dbd69847f3914e9877670ff02bfc8c01d619cb
rtof <zuichuan@msn.com>
2022-12-04 10:19:03 -0300

    adding config route

---
+++ api/routes/Config/getConfig.js
@@ -0,0 +1,15 @@
+const getConfig = require('express').Router();
+const Create = require('../../models/Create.js');
+
+getConfig.get('/', (_, res) => {
+    try {
+        if (!Create)
+          res.status(422).json({ message: 'Configuration not found!' });
+    
+        res.status(200).json(Create);
+      } catch (error) {
+        res.status(500).json({ erro: error });
+      };
+});
+
+module.exports = getConfig;
