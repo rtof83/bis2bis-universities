683e7279f538d7a505ee2b839245889522e387eb
rtof <zuichuan@msn.com>
2022-12-27 18:53:17 -0300

    adding update per hour routine

---
+++ api/services/updateHour.js
@@ -0,0 +1,6 @@
+const updateHour = () => {
+//   setInterval(() => console.log(process.env.UPDATE_HOUR), 1000 * 60 * 60 * process.env.UPDATE_HOUR);
+// setInterval(() => console.log(process.env.UPDATE_HOUR), 1000 * 5);
+};
+
+module.exports = updateHour;
