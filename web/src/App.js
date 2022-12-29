f876ccd8ecb22ef30fd39f1262128904e49488ae
rtof <zuichuan@msn.com>
2022-12-29 20:53:41 -0300

    adding contexts

--- web/src/App.js
+++ web/src/App.js
@@ -1,23 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
+
 import Home from './pages/home';
 import University from './pages/university';
-import ListUniversity from './lists/listUniversity';
-import Footer from './components/footer';
-import Header from './components/header';
+import User from './pages/user';
+import Config from './pages/config';
+import NotFound from './pages/notFound';
+
+import ListUniversity from './lists/listUniversities';
+import ListUsers from './lists/listUsers';
+
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Login from './components/Login';
+
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { SearchContext, UserContext } from './contexts/Contexts';
 
 const App = () => {
+  const [ user, setUser ] = useState([]);
+  const [ search, setSearch ] = useState([]);
+
   return (
-    <Router>
-      <Header />
-          <Routes>
-          <Route exact path='/' element={<Home />} />
-          <Route path='/university' element={<University />} />
-          <Route path='/university/:id' element={<University />} />
-          <Route path='/listUniversity' element={<ListUniversity />} />
-        </Routes>
-      <Footer />
-    </Router>
+    <SearchContext.Provider value={[ search, setSearch ]}>
+    <UserContext.Provider value={[ user, setUser ]}>
+
+      <Router>
+        <Header />
+            <Routes>
+            <Route exact path='/' element={<Home />} />
+            <Route path='/login' element={<Login />} />
+            <Route path='/listUniversity' element={<ListUniversity />} />
+
+            { user.auth && <>
+            <Route path='/user' element={<User />} />
+            <Route path='/user/:id' element={<User />} />
+
+            <Route path='/config' element={<Config />} />
+            <Route path='/listUsers' element={<ListUsers />} />
+            
+            <Route path='/university' element={<University />} />
+            <Route path='/university/:id' element={<University />} />
+            </> }
+
+            <Route path='*' exact={true} element={<NotFound />} />
+          </Routes>
+        <Footer />
+      </Router>
+
+    </UserContext.Provider>
+    </SearchContext.Provider>
   )
 };
 
---
+++ web/src/contexts/Contexts.js
@@ -0,0 +1,6 @@
+import { createContext } from 'react';
+
+const SearchContext = createContext([]);
+const UserContext = createContext([]);
+
+export { SearchContext, UserContext };
